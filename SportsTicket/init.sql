31d021edc14f54aaecb7225d6028a707483f9e09
adminqd <adminqd@e09f26c8-d46d-914d-90ba-abe2b94eece1>
2012-01-10 10:39:55 +0000

    20120110 根据业务要求,停止购彩抽奖交易

---
+++ SportsTicket/init.sql
@@ -0,0 +1,17 @@
+create table dbo.TICKETDRAW (
+   ID                   numeric(19)                    identity,
+   CrdNo                char(21)                       not null,
+   SgnMob               char(11)                       not null,
+   MobTel               char(11)                       default '' not null,
+   Drawlevel            char(1)                        default '0' not null,
+   RecvSign             char(1)                        default '0' not null,
+   NO                   numeric(19)                    default 0 not null,
+   constraint PK_LOTTDRAW primary key (ID)
+         on "default"
+)
+
+  <channel>
+      <id>441013</id>
+      <title>Με²ΚΉΊ²Κ³ι½±</title>
+      <url>/GZMBank/SportsTicket/TicketDraw1.jsp</url>
+  </channel>
