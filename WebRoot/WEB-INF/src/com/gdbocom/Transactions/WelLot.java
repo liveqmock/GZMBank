886764580dfaba207281991c6755252383c95b28
Qiming Gu <cruiser31@gmail.com>
2014-09-28 10:08:40 +0800

    完成福彩代码版本整理，将原来缺漏的java补全，目录调整

---
+++ WebRoot/WEB-INF/src/com/gdbocom/Transactions/WelLot.java
@@ -0,0 +1,10 @@
+package com.gdbocom.Transactions;
+
+public class WelLot {
+	public static final int ADDREG = 1;
+	public static final int UPDREG = 1;
+	public static final int DOUBLEBUY = 1;
+	public static final int DOUBLEFIXBUY = 1;
+	public static final int TENBUY = 1;
+
+}
