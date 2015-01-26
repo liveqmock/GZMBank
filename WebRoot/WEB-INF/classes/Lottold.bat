e24aa7c4b08d92d376ae27dec28280ebccbe2474
Qiming Gu <cruiser31@gmail.com>
2015-01-26 17:22:38 +0800

    删除classes文件夹中维护的配置文件

--- WebRoot/WEB-INF/classes/Lottold.bat
+++
@@ -1,5 +0,0 @@
-set LIB_HOME=..\lib
-set LIB_COLLECTIONS=.;%LIB_HOME%\jconn3d.jar;%LIB_HOME%\commons-net-2.0.jar;
-
-java -classpath %LIB_COLLECTIONS% com/gdbocom/report/CommonReport LottoldReport.ini
-@pause
